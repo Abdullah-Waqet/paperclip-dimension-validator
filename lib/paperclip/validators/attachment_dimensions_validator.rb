e0854e919380a44f5b9330be9fb694cb6db8a2b2
Abdullah Waqet <a.waqet@morniksa.com>
2018-08-30 13:51:15 +0300

    convert string to symbole

--- lib/paperclip/validators/attachment_dimensions_validator.rb
+++ lib/paperclip/validators/attachment_dimensions_validator.rb
@@ -21,9 +21,9 @@ module Paperclip
               record.errors.add(attribute.to_sym, :dimension, dimension_type: dimension.to_s, dimension: options[dimension], actual_dimension: dimensions.send(dimension).to_i)
             end
             pp '++++++++++++++'
-            pp options["max_#{dimension}"].to_f
+            pp options["max_#{dimension}".to_sym].to_f
             pp '++++++++++++++'
-            if options[dimension] && dimensions.send(dimension) >= options["max_#{dimension}"].to_f
+            if options[dimension] && dimensions.send(dimension) >= options["max_#{dimension}".to_sym].to_f
               pp '#_#'
               record.errors.add(attribute.to_sym,
                                 :max_dimension,
