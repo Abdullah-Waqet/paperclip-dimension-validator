20548edd0254c90bb2457035de7af0409f3d5566
Abdullah Waqet <a.waqet@morniksa.com>
2018-08-30 13:58:24 +0300

    logs

--- lib/paperclip/validators/attachment_dimensions_validator.rb
+++ lib/paperclip/validators/attachment_dimensions_validator.rb
@@ -2,7 +2,6 @@ module Paperclip
   module Validators
     class AttachmentDimensionsValidator < ActiveModel::EachValidator
       def initialize(options)
-        pp options
         super
       end
 
@@ -21,10 +20,10 @@ module Paperclip
               record.errors.add(attribute.to_sym, :dimension, dimension_type: dimension.to_s, dimension: options[dimension], actual_dimension: dimensions.send(dimension).to_i)
             end
             pp '++++++++++++++'
-            pp options["max_#{dimension}".to_sym].to_f
+            pp "max_#{dimension}".to_sym
+            pp options
             pp '++++++++++++++'
             if options[dimension] && dimensions.send(dimension) >= options["max_#{dimension}".to_sym].to_f
-              pp '#_#'
               record.errors.add(attribute.to_sym,
                                 :max_dimension,
                                 dimension_type: dimension.to_s,
